9ef6c5965024735c39d36a47ea3e2b32552c5c04
setback-king <90795553+setback-king@users.noreply.github.com>
2022-04-17 22:33:23 -0400

    responsive changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-
 import React from 'react'
 import Game from "./components/Game"
 
 
 
 function App() {
-
-
-
-
 
   return (
     <div className="App">
